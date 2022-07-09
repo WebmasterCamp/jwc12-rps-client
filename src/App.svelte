16c4a26cdddc4d73c80c7e7aa0c73597f8190154
chayapatr <chayapatr.arc@gmail.com>
2022-07-09 18:58:44 +0700

    feat: add connect status

--- src/App.svelte
+++ src/App.svelte
@@ -5,9 +5,19 @@
     "https://9d48-2001-44c8-428c-82d6-b077-bd9b-34af-5647.ap.ngrok.io",
     { transports: ["websocket"] }
   );
+
+  let isConnect = true;
+
   console.log(socket);
-  socket.on("portal", (arg) => {
+  socket.on("connect", (arg) => {
+    socket.emit("portal", socket.id);
     console.log(arg);
+  });
+
+  socket.on("status", (arg) => {
+    if (socket.id !== arg) {
+      isConnect = false;
+    }
   });
 
   const resolve = (result) => {
@@ -22,30 +32,34 @@
 <Router>
   <Route path="/">
     <main class="flex w-[100vw] h-[100vh] justify-center items-center">
-      <div class="flex flex-wrap">
-        <div class="w-full flex justify-center items-center">
-          <button
-            on:click={() => {
-              resolve("🪨");
-            }}
-            class="btn">🪨</button
-          >
-          <button
-            on:click={() => {
-              resolve("📄");
-            }}
-            class="btn">📄</button
-          >
+      {#if isConnect}
+        <div class="flex flex-wrap">
+          <div class="w-full flex justify-center items-center">
+            <button
+              on:click={() => {
+                resolve("🪨");
+              }}
+              class="btn">🪨</button
+            >
+            <button
+              on:click={() => {
+                resolve("📄");
+              }}
+              class="btn">📄</button
+            >
+          </div>
+          <div class="flex w-full justify-center items-center">
+            <button
+              on:click={() => {
+                resolve("✂️");
+              }}
+              class="btn">✂️</button
+            >
+          </div>
         </div>
-        <div class="flex w-full justify-center items-center">
-          <button
-            on:click={() => {
-              resolve("✂️");
-            }}
-            class="btn">✂️</button
-          >
-        </div>
-      </div>
+      {:else}
+        <h1 class="text-4xl">Disconnected</h1>
+      {/if}
     </main>
   </Route>
   <Route path="tofu-eating-cat">
